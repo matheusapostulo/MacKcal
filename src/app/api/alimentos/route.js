ead848e2443f012facdd52348d5d1414a1199043
matheusapostulo <matheusapostulo10@gmail.com>
2024-05-09 14:35:53 -0300

    Retirando search params do try-catch

--- src/app/api/alimentos/route.js
+++ src/app/api/alimentos/route.js
@@ -1,11 +1,10 @@
 const { NextResponse } = require("next/server");
 
 export async function GET(req){
+    // Obtendo a query pelo search params
+    const request_query = req.nextUrl.searchParams
+    const query = request_query.get("query")
     try {
-        // Obtendo a query pelo search params
-        const request_query = req.nextUrl.searchParams
-        const query = request_query.get("query")
-
         // Vamos fazer o fetch na api de alimentos
         const res = await fetch(`https://caloriasporalimentoapi.herokuapp.com/api/calorias/?descricao=${query}`);
 
